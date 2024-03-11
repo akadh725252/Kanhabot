e87459e67972dbc739bafa71da48d407453b81be
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-03-12 01:58:49 +0530

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -12,6 +12,14 @@ __doc__ = get_help("help_bot")
 import os
 import sys
 import time
+import asyncio
+from random import randrange
+from re import search
+
+from telethon.events import NewMessage, MessageEdited
+from telethon.errors import DataInvalidError
+
+from . import *
 from platform import python_version as pyver
 from random import choice
 
@@ -354,3 +362,111 @@ async def updava(event):
         force_document=False,
         buttons=Button.inline("Changelogs", data="changes"),
     )
+
+HEXA_ID = 572621020
+
+
+async def re_fetch(m):
+    return await m.client.get_messages(m.chat_id, ids=m.id)
+
+
+async def watch_edits(chat, msg_id, timeout=20):
+    async with ultroid_bot.conversation(chat, timeout=timeout) as conv:
+        func = lambda e: e.id == msg_id and search(rf"(?i)Current turn: (.+){ultroid_bot.uid}", e.message.text)
+        response = conv.wait_event(
+            MessageEdited(
+                incoming=True,
+                from_users=HEXA_ID,
+                func=func,
+            )
+        )
+        response = await response
+        await asyncio.sleep(3)
+        response = await re_fetch(response)
+        return response
+
+
+# try clicking button 3 times in background. (2s delay)
+async def do_click(msg, *buttons):
+    async def _click():
+        try:
+            await msg.click(*buttons)
+        except DataInvalidError:
+            pass
+
+    async def _loop():
+        nonlocal msg
+        for _ in range(3):
+            await asyncio.sleep(2)
+            r_msg = await re_fetch(msg)
+            if r_msg and r_msg.buttons and r_msg.text == msg.text:
+                await _click()
+                msg = r_msg
+            else:
+                return
+
+    await _click()
+    asyncio.create_task(_loop())
+
+
+# clicks ready button
+async def get_ready(chat):
+    async with ultroid_bot.conversation(chat, timeout=16) as conv:
+        response = conv.wait_event(
+            NewMessage(incoming=True, from_users=HEXA_ID, func=lambda e: e.mentioned)
+        )
+        response = await response
+        if response.buttons and response.buttons[0][0].text.startswith("Ready"):
+            await asyncio.sleep(3)
+            await do_click(response, 0, 0)
+            await asyncio.sleep(3)
+            return response.id
+
+
+# clicks button b/w (2-6)
+async def click_rnd_button(msg):
+    for count1, sub_buttons in enumerate(msg.buttons):
+        for count2, button in enumerate(sub_buttons):
+            text = str(button.text).strip()
+            if text and text.isdigit():
+                return await do_click(msg, count1, count2)
+
+
+# click (attack) if dodged or missed otherwise pokemon -> 2 to 6
+async def auto_battle(chat, msg_id):
+    response = await watch_edits(chat, msg_id, timeout=15)
+    await asyncio.sleep(2)
+    if not response.buttons:
+        return True
+    elif "team" in response.buttons[0][0].text.lower():
+        return await click_rnd_button(response)
+    else:
+        rndm = lambda: randrange(2)
+        return await do_click(response, rndm(), rndm())
+
+
+@kanha_bot.on(
+    NewMessage(
+        incoming=True,
+        pattern="^/fakeChallenge$",
+        from_users=udB.get_key("SUDOS"),
+    )
+)
+async def autohemxa(e):
+    await asyncio.sleep(1)
+    msg = await e.reply("#ReadyforBattle")
+    try:
+        hexa_msg_id = await get_ready(e.chat_id)
+        await msg.delete()
+        while True:
+            response = await auto_battle(e.chat_id, hexa_msg_id)
+            # await asyncio.sleep(0.3)
+            if response == True:
+                return
+    except asyncio.TimeoutError:
+        return await msg.respond("Timeout Error.. Skipping!")
+    except Exception as exc:
+        LOGS.exception(exc)
+        await msg.respond(f"**2nd ID Error** \n\nGot {exc.__class__} \n`{exc}`")
+
+
