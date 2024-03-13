aacdb980de43e91c25e4dca8d82114dec562bb7a
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-03-13 21:42:28 +0530

    Add files via upload

---
+++ plugins/r (1).py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+import asyncio
+from random import randrange
+from re import search
+from telethon.tl.functions.messages import EditMessageRequest
+from telethon.tl.custom import Message
+from telethon import TelegramClient
+from telethon import events
+
+@kanha_cmd(
+    pattern="r$",
+    fullsudo=True,
+)
+async def restart(event):
+    await event.edit("Restarting...")
+    os.execl(sys.executable, sys.executable, *sys.argv)
