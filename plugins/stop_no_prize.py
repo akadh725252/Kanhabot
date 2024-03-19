578a5e20d8477ae6d45c8b5b8acf0fc81dae17b7
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-03-19 23:37:01 +0530

    Delete plugins/stop_no_prize.py

--- plugins/stop_no_prize.py
+++
@@ -1,8 +0,0 @@
-from telethon import TelegramClient, events
-import asyncio
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from . import *
-@kanha_bot.on(events.MessageEdited)
-async def my_event_handler(event):
-      if 'Daily limit for battling has been reached, no prize will be given' in event.raw_text:
-               await restart(event)
